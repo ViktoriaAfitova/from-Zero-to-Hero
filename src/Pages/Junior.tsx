9d7a742c922a142951c9bc74d6788ec7a137380f
Viktoria Afitova <viktoria.afitova@alignedcode.com>
2023-05-18 15:22:03 +0300

    Add header for junior page

--- src/Pages/Junior.tsx
+++ src/Pages/Junior.tsx
@@ -1,3 +1,8 @@
 export const Junior = () => {
-  return <div>Junior</div>;
+  return (
+    <>
+      <header className='flex justify-center py-10 bg-grayDark' />
+      <div>Theory</div>
+    </>
+  );
 };
